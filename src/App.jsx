508ecfe68d61efda40727b99cf155bdefa9e24f9
AYOBAMI-JOSHUA <ayobamij89@gmail.com>
2025-09-09 21:19:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Joshua Akodu Ayobami</h1>
+      
+      <p className="read-the-docs">
+        Creating my portfolio project
+      </p>
+    </>
+  )
+}
+
+export default App
